e3b2db57b2db9c8f19f1c3cc39a7604c76cc072a
David Grosh <dovidgrosh98@gmail.com>
2019-07-16 00:33:10 -0400

    added high score and instructions

--- start.js
+++ start.js
@@ -1,3 +1,11 @@
 document.querySelector('#start').addEventListener('click', () => {
   window.location.href = 'level-1/level-1.html';
 });
+document.querySelector('#instruction').addEventListener('click', () => {
+  document.querySelector('#instruction-screen').style.opacity = '1';
+  document.querySelector('#instruction-screen').style.zIndex = '12';
+  document.querySelector('#read-instructions').addEventListener('click', () => {
+    document.querySelector('#instruction-screen').style.opacity = '0';
+    document.querySelector('#instruction-screen').style.zIndex = '-1';
+  });
+});
